7882956e14115441610064e84a410eb7e4f9bb2f
samhiggins2001 <higgins2001@hotmail.com>
2014-06-04 16:48:52 -0700

    whatever -- this is just the build and run script

--- executionDir/buildAndRunScript.R
+++ executionDir/buildAndRunScript.R
@@ -3,22 +3,30 @@ require("roxygen2")
 # dir.create("/Users/samhiggins2001_worldperks/tprog/distribution/pathLayerDistributionVersion2/executionDir")
 setwd("/Users/samhiggins2001_worldperks/tprog/distribution/pathLayerDistributionVersion2/executionDir")
 
-roxygenize(overwrite=T, package.dir="/Users/samhiggins2001_worldperks/tprog/distribution/pathLayerDistributionVersion2/packageDir/")
-roxygenize()
 detach("package:packageDir", unload=TRUE)
 remove.packages("packageDir")
-readline("please run R CMD build packageDir then press enter")
-# cd ~/tprog/distribution/pathLayerDistributionVersion2
-#
 
-install.packages("/Users/samhiggins2001_worldperks/tprog/distribution/pathLayerDistributionVersion2/packageDir_0.1.tar.gz", repos = NULL, type = "source")
+roxygenize(overwrite=T, 
+					 package.dir="/Users/samhiggins2001_worldperks/tprog/distribution/pathLayerDistributionVersion2/packageDir/")
+
+# roxygenize()
+
+readline("please run R CMD build packageDir then press enter")
+library("packageDir", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
+# cd ~/tprog/distribution/pathLayerDistributionVersion2
+
+# install.packages("/Users/samhiggins2001_worldperks/tprog/distribution/pathLayerDistributionVersion2/packageDir_0.1.tar.gz", repos = NULL, type = "source")
 
 # devtools::install("/Users/samhiggins2001_worldperks/tprog/distribution/pathLayerDistributionVersion2/packageDir")
 library("packageDir")
-STUDY = runInteractivePathAnalysis()
+STUDY = allInteractiveMainFunction()
 
 library("devtools")
-load_all("packageDir")
+load_all(pkg="packageDir", export_all=T, reset=T)
+
+
+packageDir:list_to_table()
+
 
 # PACKAGENAME="packageDir"
 # data("defaultSummaryTable140504")
@@ -39,5 +47,31 @@ load_all("packageDir")
 #'																																				arms=stud@arms)
 #'																										return(stud)
 #'																									})}
-#'																									
 
+
+# improved list of objects
+.ls.objects <- function (pos = 1, pattern, order.by,
+												 decreasing=FALSE, head=FALSE, n=5) {
+	napply <- function(names, fn) sapply(names, function(x)
+		fn(get(x, pos = pos)))
+	names <- ls(pos = pos, pattern = pattern)
+	obj.class <- napply(names, function(x) as.character(class(x))[1])
+	obj.mode <- napply(names, mode)
+	obj.type <- ifelse(is.na(obj.class), obj.mode, obj.class)
+	obj.size <- napply(names, object.size)
+	obj.dim <- t(napply(names, function(x)
+		as.numeric(dim(x))[1:2]))
+	vec <- is.na(obj.dim)[, 1] & (obj.type != "function")
+	obj.dim[vec, 1] <- napply(names, length)[vec]
+	out <- data.frame(obj.type, obj.size, obj.dim)
+	names(out) <- c("Type", "Size", "Rows", "Columns")
+	if (!missing(order.by))
+		out <- out[order(out[[order.by]], decreasing=decreasing), ]
+	if (head)
+		out <- head(out, n)
+	out
+}
+# shorthand
+lsos <- function(..., n=10) {
+	.ls.objects(..., order.by="Size", decreasing=TRUE, head=TRUE, n=n)
+}
