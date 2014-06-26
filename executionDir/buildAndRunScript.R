262ab0b01136484abad55a4219a03387a9a0009d
samhiggins2001 <higgins2001@hotmail.com>
2014-06-25 18:14:29 -0700

    made this nicer to look at / use

--- executionDir/buildAndRunScript.R
+++ executionDir/buildAndRunScript.R
@@ -10,8 +10,8 @@ roxygenize(overwrite=T,
 
 # roxygenize()
 
-readline("please run R CMD build packageDir then press enter")
-library("packageDir", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
+# readline("please run R CMD build packageDir then press enter")
+# library("packageDir", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
 # cd ~/tprog/distribution/pathLayerDistributionVersion2
 
 # install.packages("/Users/samhiggins2001_worldperks/tprog/distribution/pathLayerDistributionVersion2/packageDir_0.1.tar.gz", repos = NULL, type = "source")
@@ -20,14 +20,37 @@ library("packageDir", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Reso
 library("packageDir")
 STUDY = allInteractiveMainFunction()
 
-library("devtools")
-load_all(pkg="packageDir", export_all=T, reset=T)
 
-packageDir:::list_to_table()
+sourceAllInFolder<-function(folname="../packageDir/R/"){
+	
+	if(!grepl(pattern="/$", x=folname)) folname = paste0(folname,"/")
+	
+	fnames = dir(folname)
+	fnames = fnames[grep(pattern="r", x=fnames, ignore.case=T)]
+	fnames = paste0(folname, fnames)
+	for(fn in fnames){
+		cat("..",fn,"..")
+		source(fn)
+	}
+	
+}
+sourceAllInFolder()
 
 
-abacavirIntegrationTest = STUDY
 
+
+
+
+# 
+# 
+# library("devtools")
+# load_all(pkg="packageDir", export_all=T, reset=T)
+# 
+# packageDir:::list_to_table()
+# 
+# 
+# abacavirIntegrationTest = STUDY
+# 
 
 
 # PACKAGENAME="packageDir"
@@ -50,44 +73,31 @@ abacavirIntegrationTest = STUDY
 #'																										return(stud)
 #'																									})}
 
-sourceAllInFolder<-function(folname="../packageDir/R/"){
-	
-	if(!grepl(pattern="/$", x=folname)) folname = paste0(folname,"/")
-	
-	fnames = dir(folname)
-	fnames = fnames[grep(pattern="r", x=fnames, ignore.case=T)]
-	fnames = paste0(folname, fnames)
-	for(fn in fnames){
-		cat("..",fn,"..")
-		source(fn)
-	}
-	
-}
-sourceAllInFolder()
+
 
 # improved list of objects
-.ls.objects <- function (pos = 1, pattern, order.by,
-												 decreasing=FALSE, head=FALSE, n=5) {
-	napply <- function(names, fn) sapply(names, function(x)
-		fn(get(x, pos = pos)))
-	names <- ls(pos = pos, pattern = pattern)
-	obj.class <- napply(names, function(x) as.character(class(x))[1])
-	obj.mode <- napply(names, mode)
-	obj.type <- ifelse(is.na(obj.class), obj.mode, obj.class)
-	obj.size <- napply(names, object.size)
-	obj.dim <- t(napply(names, function(x)
-		as.numeric(dim(x))[1:2]))
-	vec <- is.na(obj.dim)[, 1] & (obj.type != "function")
-	obj.dim[vec, 1] <- napply(names, length)[vec]
-	out <- data.frame(obj.type, obj.size, obj.dim)
-	names(out) <- c("Type", "Size", "Rows", "Columns")
-	if (!missing(order.by))
-		out <- out[order(out[[order.by]], decreasing=decreasing), ]
-	if (head)
-		out <- head(out, n)
-	out
-}
-# shorthand
-lsos <- function(..., n=10) {
-	.ls.objects(..., order.by="Size", decreasing=TRUE, head=TRUE, n=n)
-}
+# .ls.objects <- function (pos = 1, pattern, order.by,
+# 												 decreasing=FALSE, head=FALSE, n=5) {
+# 	napply <- function(names, fn) sapply(names, function(x)
+# 		fn(get(x, pos = pos)))
+# 	names <- ls(pos = pos, pattern = pattern)
+# 	obj.class <- napply(names, function(x) as.character(class(x))[1])
+# 	obj.mode <- napply(names, mode)
+# 	obj.type <- ifelse(is.na(obj.class), obj.mode, obj.class)
+# 	obj.size <- napply(names, object.size)
+# 	obj.dim <- t(napply(names, function(x)
+# 		as.numeric(dim(x))[1:2]))
+# 	vec <- is.na(obj.dim)[, 1] & (obj.type != "function")
+# 	obj.dim[vec, 1] <- napply(names, length)[vec]
+# 	out <- data.frame(obj.type, obj.size, obj.dim)
+# 	names(out) <- c("Type", "Size", "Rows", "Columns")
+# 	if (!missing(order.by))
+# 		out <- out[order(out[[order.by]], decreasing=decreasing), ]
+# 	if (head)
+# 		out <- head(out, n)
+# 	out
+# }
+# # shorthand
+# lsos <- function(..., n=10) {
+# 	.ls.objects(..., order.by="Size", decreasing=TRUE, head=TRUE, n=n)
+# }
